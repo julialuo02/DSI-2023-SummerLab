1cbb6ba97fc150b5bd8fcf44e54ca9e20b344beb
Julia Luo <julialuo@Julias-MacBook-Pro.local>
2023-07-31 16:59:24 -0500

    Projectile dataset changes

--- projectile.py
+++ projectile.py
@@ -14,6 +14,9 @@ MAX_LAUNCH_ANGLE = 85
 MIN_LAUNCH_HEIGHT = 2
 MAX_LAUNCH_HEIGHT = 15
 
+if not os.path.exists('./projectile_data'):
+    os.makedirs('./projectile_data')
+
 def calculate_trajectory(angle, height):
 
     angle_rad = np.radians(angle)
@@ -30,4 +33,30 @@ def calculate_trajectory(angle, height):
 
     while y >= 0:
         
-        
+        time += TIME_STEPS
+        x = speed_x * time
+        y = height + speed_y * 0.5 * GRAVITY * (time ** 2)
+
+        x_values.append(x)
+        y_values.append(y)
+
+num_samples = 10
+
+for i in range(num_samples):
+
+    angle_deg = np.random.uniform(MIN_LAUNCH_ANGLE, MAX_LAUNCH_ANGLE)
+    height = np.random.uniform(MIN_LAUNCH_HEIGHT, MAX_LAUNCH_HEIGHT)
+
+    x_values, y_values = calculate_trajectory(angle_deg, height)
+    distance_traveled = max(x_values)
+
+    plt.figure()
+    plt.plot(x_values, y_values)
+    
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.grid(False)
+    plt.axis('off')
+
+
+
